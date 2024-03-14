4d38ee66086e980679a3b8e9f55b94deba8e28f0
Meredith <mbellomy81@gmail.com>
2024-03-14 19:25:04 -0400

    changes to djangoapp and djangoproj folders

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-
 # Create your models here.
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -14,11 +13,10 @@ class CarMake(models.Model):
     ]
 
     def __str__(self):
-        return self.name  
-
+        return self.name
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) 
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -28,11 +26,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-            validators=[
+        validators=[
             MaxValueValidator(2023),
-            MinValueValidator(2015)
+            MinValueValidator(2015),  
         ])
-    # Other fields as needed
+# Other fields as needed
 
-    def __str__(self):
-        return self.name  
+def __str__(self):
+        return self.name
