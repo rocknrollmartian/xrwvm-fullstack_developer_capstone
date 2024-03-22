0f7d2650a8a6108df75c98ad0b352f41f16c55ea
Meredith <mbellomy81@gmail.com>
2024-03-22 17:52:07 -0400

    Changes added to python files

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,8 +17,7 @@ urlpatterns = [
 
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
 
-    path(route='get_dealers/<str:state>',
-         view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'), # noqa
 
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'), # noqa
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -23,8 +23,7 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name,
-                    "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name}) # noqa
     return JsonResponse({"CarModels": cars})
 
 
